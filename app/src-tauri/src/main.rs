7ff863ab02772a19b77f26991adff1744911a14e
Si-Yan Teo <dev@teosiyan.com>
2025-06-05 22:42:55 +0800

    chore(app): Add dotenv crate to load env vars from .env file

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenv::dotenv;
+
 fn main() {
+    dotenv().ok(); // This line loads the environment variables from the ".env" file.
     hledger_app_lib::run()
 }
